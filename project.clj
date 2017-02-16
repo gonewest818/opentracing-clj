9ac98dd35fa55c545de945f27dac6893655855b6
Neil Okamoto <neil.okamoto@gmail.com>
2017-02-15 18:31:23 -0800

    clean up lein configuration

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject opentracing-clj "0.1.0-SNAPSHOT"
+(defproject org.clojars.gonewest818/opentracing-clj "0.1.0-SNAPSHOT"
   :description "OpenTracing bindings for Clojure"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/gonewest818/opentracing-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
