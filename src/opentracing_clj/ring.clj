767222b372b73037c2f8ac53befb25992e28df50
Neil Okamoto <neil.okamoto@gmail.com>
2017-02-16 08:35:15 -0800

    Ring middleware needs to be passed the tracer explicitly

--- src/opentracing_clj/ring.clj
+++ src/opentracing_clj/ring.clj
@@ -6,11 +6,11 @@
   "ring middleware creates a span from the inbound request
   with the given name and optional tags, then leaves the tracing
   context in the request dictionary as :opentracing-context"
-  ([handler op-name]
-   (wrap-opentracing handler op-name nil))
-  ([handler op-name tags]
+  ([handler tracer op-name]
+   (wrap-opentracing handler tracer op-name nil))
+  ([handler tracer op-name tags]
    (fn [request]
      ;;(log/debug (:headers request))
-     (let [ctx (ot/http->context (:headers request))]
-       (ot/with-trace [s (ot/child-span ctx op-name tags)]
+     (let [ctx (ot/http->context tracer (:headers request))]
+       (ot/with-trace [s (ot/child-span tracer ctx op-name tags)]
          (handler (assoc request :opentracing-context s)))))))
